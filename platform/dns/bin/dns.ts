7099a36b7b90cf88517c5a6a89f39676b19be0b6
Karl Merecido <kmerecido@ostusa.com>
2026-09-07 22:06:11 -0500

    Scaffold platform/dns: Route 53 hosted zone for kkbae.com

---
+++ platform/dns/bin/dns.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { DnsStack } from '../lib/dns-stack';
+
+const app = new cdk.App();
+
+new DnsStack(app, 'MoneyBaeDns', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-1',
+  },
+});
---
+++ platform/dns/lib/dns-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import { Construct } from 'constructs';
+
+export class DnsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const zone = new route53.HostedZone(this, 'KkbaeZone', {
+      zoneName: 'kkbae.com',
+    });
+
+    new cdk.CfnOutput(this, 'HostedZoneId', { value: zone.hostedZoneId });
+    new cdk.CfnOutput(this, 'NameServers', {
+      value: cdk.Fn.join(', ', zone.hostedZoneNameServers!),
+    });
+  }
+}
